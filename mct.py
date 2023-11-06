0e4d4ea966d0534be74e2a82e25422ea49af9182
Marco Barbero Mota <marco.barbero.mota@vanderbilt.edu>
2023-11-06 06:01:44 -0600

    numpy.bool,int etc are deprecated

--- mct.py
+++ mct.py
@@ -54,7 +54,7 @@ def histograms(probs, actual, bins=100):
     Calculates two histograms over [0, 1] by partitioning `probs` with `actual`
     and sorting each partition into `bins` sub-intervals.
     """
-    actual = actual.astype(np.bool)
+    actual = actual.astype(bool)
     edges, step = np.linspace(0., 1., bins, retstep=True, endpoint=False)
     idx = np.digitize(probs, edges) - 1
     top = np.bincount(idx, weights=actual, minlength=bins)
